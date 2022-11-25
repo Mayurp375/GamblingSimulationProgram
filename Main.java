142c87f87e59216cec95fa5aa86983e568465be5
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-25 10:38:14 +0530

    uc1

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to gambling simulator game");
+        uc1 uc1 = new uc1();
+        uc1.start();
     }
 }
---
+++ uc1.java
@@ -0,0 +1,12 @@
+import javax.print.attribute.standard.RequestingUserName;
+import java.util.Random;
+
+public class uc1 {
+    Random random = new Random();
+
+    void start(){
+        int stakeOfDay = 100;
+        int betEveryGame = 1;
+
+    }
+}
